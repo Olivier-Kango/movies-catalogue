a5e0d49267ec294ec344d1a9cb4a52e5550fff86
Olivier-Kango <olivierkango@gmail.com>
2022-12-01 13:25:51 -0800

    Create test for Filter

---
+++ src/__test__/Filter.test.js
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import YearsFilter from '../components/Filter/Filter';
+import Movies from '../components/Movies/Movies';
+import store from '../redux/configureStore';
+
+describe('Testing the Years Filter', () => {
+  test('Should render', () => {
+    render(
+      <Provider store={store}>
+        <Movies>
+          <YearsFilter />
+        </Movies>
+      </Provider>
+    );
+  });
+});

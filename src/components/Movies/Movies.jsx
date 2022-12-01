08d0f8248d9f61773b568e183c86953e38a02cda
Olivier-Kango <olivierkango@gmail.com>
2022-12-01 09:24:59 -0800

    Add props in Filter component

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMovies } from '../../redux/movies/movies';
 import Movie from './Movie';
+import YearsFilter from '../Filter/Filter';
 import './Movies.css';
 
 const Movies = () => {
@@ -10,32 +11,45 @@ const Movies = () => {
   const dispatch = useDispatch();
   useEffect(() => { if (moviess.length === 0) { dispatch(fetchMovies('Home')); } }, []);
 
-  const movies = Array.from(moviess).sort((a, b) => b.popularity - a.popularity);
+  const moviesAll = Array.from(moviess).sort((a, b) => b.popularity - a.popularity);
+  const [filterTextValue, updateFilterText] = useState('All');
 
+  const movies = moviesAll.filter((movie) => {
+    if (filterTextValue === 'All') {
+      return movie;
+    }
+    return filterTextValue === movie.release_date.split('-')[0];
+  });
+
+  console.log(movies);
   const ref = useRef(null);
   const handleClick = (e) => {
     e.preventDefault();
     dispatch(fetchMovies(ref.current.value));
   };
 
+  const onFilterValueSelected = (filterValue) => {
+    updateFilterText(filterValue);
+  };
+
   return (
     <div>
-      <navbar className="navbar">
-        <form onSubmit={handleClick}>
-          <input
-            type="text"
-            id="search"
-            name="search"
-            ref={ref}
-          />
-          <button
-            type="submit"
-            id="button-search"
-          >
-            Search
-          </button>
-        </form>
-      </navbar>
+      <h1>Family Movies Catalogue</h1>
+      <form onSubmit={handleClick}>
+        <input
+          type="text"
+          id="search"
+          name="search"
+          ref={ref}
+        />
+        <button
+          type="submit"
+          id="button-search"
+        >
+          Search
+        </button>
+      </form>
+      <YearsFilter loadedMovies={moviesAll} filterValueSelected={onFilterValueSelected} />
       <br className="br" />
       {movies.map((movie) => (
         <div key={`${movie.id}`} id={movie.id}>
